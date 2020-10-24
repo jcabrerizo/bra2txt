59c007430984a38a8b58645ab65854b265bcd636
Juan Cabrerizo <juan@cloudsoft.io>
2020-10-24 12:59:01 +0100

    Fix path problems

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
     version='0.1.0',
     description='Converter for print-braille text to plain txt',
     long_description=readme,
+    long_description_content_type="text/markdown",
     author='Juan D. Cabrerizo',
     url="https://github.com/jcabrerizo/bra2txt",
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=['bra2txt'],
     entry_points={
         'console_scripts': [
             'bra2txt=bra2txt.cli:main'
